61cd106b0a60687cc00912e18277d83f1ff9313d
tdu9 <108372516+tdu9@users.noreply.github.com>
2022-08-21 08:46:33 -0400

    Add files via upload

---
+++ bowtie.py
@@ -0,0 +1,13 @@
+n = input('Enter the size of a side of the bow-tie: ')
+n = int(n)
+for i in range(n):
+    string = '*' * (i + 1)
+    string += ' ' * (2 * (n - i - 1))
+    string += '*' * (i + 1)
+    print(string)
+for i in range(n - 1):
+    string = '*' * (n - i - 1)
+    string += ' ' * (2 * (i + 1))
+    string += '*' * (n - i - 1)
+    print(string)
+    
